bfb679dc28f7b07fd9a6667124159e37ca6407ee
JB <jadon.binda@gmail.com>
2023-10-11 11:40:38 -0300

    feat: adicionada rota para a página de detalhamento do produto

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Main from './pages/Main'
+import ProductDetail from './pages/ProductDetail'
 
 function MainRoutes() {
   return (
     <Routes>
       <Route path='/' element={<Main />} />
+      <Route path='/product' element={<ProductDetail />} />
     </Routes>
   )
 }
