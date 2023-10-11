bf9f50cc1a391256d0d5b78380db6de759b0a795
JB <jadon.binda@gmail.com>
2023-10-11 08:05:44 -0300

    feat: implementação da página principal integrando com o backend para exibição da lista de produtos

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,39 @@
 import './styles.css';
-import Header from '../../components/Header'
+import Header from '../../components/Header';
+import ProductCard from '../../components/ProductCard';
+import api from '../../services/api'
+import { useEffect, useState } from 'react';
+import Product from '../../types/Product';
 
 function Main() {
+  const [allProducts, setAllProducts] = useState<Product[]>([]);
+
+  async function loadProducts() {
+    try {
+      const response = await api.get('/product');
+
+      setAllProducts([...response.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
 
   return (
     <div className="container">
       <Header />
+
+      <div className='container-products'>
+        {allProducts.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </div>
     </div>
   );
 }
