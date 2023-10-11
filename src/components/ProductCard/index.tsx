f94763831b9f5d79af2b927e0a4621a419be5526
JB <jadon.binda@gmail.com>
2023-10-11 11:33:53 -0300

    feat: criação de navegação para a página de detalhes do produto e implementação do armazenamento das informações do produto no localStorage

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,15 +1,28 @@
-import './styles.css';
-import Product from '../../types/Product';
+import './styles.css'
+import Product from '../../types/Product'
+import { useNavigate } from 'react-router-dom'
+import useProductDetail from '../../hooks/useProductDetail'
+import ProductImage from '../ProductImage'
 
 type Props = {
   product: Product
 }
 
 export default function ProductCard({ product }: Props) {
+  const navigate = useNavigate()
+  const { handleInsertProduct } = useProductDetail()
+
+  function handleProductDetail() {
+    handleInsertProduct(product)
+    navigate('/product')
+  }
 
   return (
-    <div className='product-card'>
-      <img src={product.imageURL} alt='computador' className='product-image' />
+    <div
+      className='product-card'
+      onClick={handleProductDetail}
+    >
+      <ProductImage image={product.imageURL} name={product.name} />
       <h1>{product.name}</h1>
     </div>
   )
